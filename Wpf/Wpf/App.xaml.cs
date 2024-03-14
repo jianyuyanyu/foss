7f95f98c46618bd0ccbb3520e1bce25b4fdfe0e7
Joe DeCock <josephdecock@gmail.com>
2024-03-14 12:40:20 -0500

    Add DPOP to WPF sample

--- Wpf/Wpf/App.xaml.cs
+++ Wpf/Wpf/App.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using IdentityModel.Client;
 using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.DPoP;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,11 +18,13 @@ namespace Wpf;
 /// </summary>
 public partial class App : Application
 {
+    static readonly string Api = "https://demo.duendesoftware.com/api/dpop/test";
     internal static string[] Args = null;
     internal static string CustomUriScheme = "oidcclient-wpf-sample";
 
-    private OidcClient _client;
+    private OidcClient _oidcClient;
     private AuthorizeState _state;
+    internal HttpClient _apiClient;
 
     protected override async void OnStartup(StartupEventArgs e)
     {
@@ -36,19 +43,53 @@ public partial class App : Application
             var mainWindow = new MainWindow();
             mainWindow.Show();
 
-            // Begin OAuth Flow
-            await InitiateChallenge();
+            var proofKey = InitializeOidcClient();
 
-            // Wait for the user to login
-            var result = await ReceiveCallback();
+            var (refreshToken, userId) = GetSession();
+            if (refreshToken != null)
+            {
+                InitializeApiClient(proofKey, refreshToken);
 
-            // Marshall the update into the UI
-            mainWindow.Dispatcher.Invoke(() => mainWindow.UserLoggedIn(result));
+                // Marshall an update into the UI
+                mainWindow.Dispatcher.Invoke(() =>
+                {
+                    mainWindow.DisplayMessage($"User previously logged in: {userId}");
+                });
+            }
+            else
+            {
+                _state = await _oidcClient.PrepareLoginAsync();
+
+                // open system browser to start authentication
+                OpenBrowser(_state.StartUrl);
+
+                // Wait for the user to login
+                var result = await ReceiveCallback();
+
+                InitializeApiClient(result);
+                StoreSession(result.RefreshToken, result.User.Identity.Name);
+
+                // Marshall the update into the UI
+                mainWindow.Dispatcher.Invoke(() =>
+                {
+                    mainWindow.DisplayMessage($"User logged in: {result.User.Identity.Name}");
+                });
+            }
+            mainWindow.Initialize(_apiClient, _oidcClient);
         }
         base.OnStartup(e);
     }
 
-    private async Task InitiateChallenge()
+    private void OpenBrowser(string url)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = url,
+            UseShellExecute = true
+        });
+    }
+
+    private string InitializeOidcClient()
     {
         // Configure OidcClient and prepare a login request
         string redirectUri = $"{App.CustomUriScheme}://callback";
@@ -56,27 +97,43 @@ public partial class App : Application
         {
             Authority = "https://demo.duendesoftware.com/",
             ClientId = "interactive.public",
-            Scope = "openid profile email",
+            Scope = "openid profile email offline_access",
             RedirectUri = redirectUri,
+            PostLogoutRedirectUri = "" // ???
         };
-        _client = new OidcClient(options);
-        _state = await _client.PrepareLoginAsync();
 
-        // open system browser to start authentication
-        Process.Start(new ProcessStartInfo
+        // Enable DPoP
+        var proofKey = GetProofKey();
+        options.ConfigureDPoP(proofKey);
+
+        _oidcClient = new OidcClient(options);
+        return proofKey;
+    }
+
+    private void InitializeApiClient(string proofKey, string refreshToken)
+    {
+        // Use saved refresh token
+        var handler = _oidcClient.CreateDPoPHandler(proofKey, refreshToken);
+        _apiClient = new HttpClient(handler)
         {
-            FileName = _state.StartUrl,
-            UseShellExecute = true
-        });
+            BaseAddress = new Uri(Api)
+        };
+    }
+
+    private void InitializeApiClient(LoginResult result)
+    {
+        _apiClient = new HttpClient(result.RefreshTokenHandler)
+        {
+            BaseAddress = new Uri(Api)
+        };
     }
 
     private async Task<LoginResult> ReceiveCallback()
     {
         var callbackManager = new CallbackManager(_state.State);
         var response = await callbackManager.RunServer();
-        return await _client.ProcessResponseAsync(response, _state);
+        return await _oidcClient.ProcessResponseAsync(response, _state);
     }
-
 
     private async Task SendCallbackToMainProcess(string args)
     {
@@ -86,5 +143,51 @@ public partial class App : Application
             var callbackManager = new CallbackManager(response.State);
             await callbackManager.RunClient(args);
         }
+    }
+
+    private string GetProofKey()
+    {
+        if (File.Exists("proofkey"))
+        {
+            var protectedKey = File.ReadAllText("proofkey");
+            return Unprotect(protectedKey);
+        }
+
+        var proofKey = JsonWebKeys.CreateRsaJson();
+        File.WriteAllText("proofkey", Protect(proofKey));
+        return proofKey;
+    }
+
+    // Our "session" is just the username and refresh token.
+    // If we cared about more claims, we could write those to disk as well.
+    private (string refreshToken, string username) GetSession()
+    {
+        if(File.Exists("refresh_token"))
+        {
+            var protectedRefreshToken = File.ReadAllText("refresh_token");
+            var unprotected = Unprotect(protectedRefreshToken);
+            var split = unprotected.Split(",");
+            return (split[0], split[1]);
+        }
+        return (null, null);
+    }
+
+    private void StoreSession(string refreshToken, string username)
+    {
+        var plainText = $"{refreshToken},{username}";
+        File.WriteAllText("refresh_token", Protect(plainText));
+    }
+
+    private string Protect(string plainText)
+    {
+        byte[] encryptedData = ProtectedData.Protect(Encoding.UTF8.GetBytes(plainText), null, DataProtectionScope.CurrentUser);
+        return Convert.ToBase64String(encryptedData);
+    }
+
+    private string Unprotect(string encryptedText)
+    {
+        var encryptedData = Convert.FromBase64String(encryptedText);
+        var decryptedData = ProtectedData.Unprotect(encryptedData, null, DataProtectionScope.CurrentUser);
+        return Encoding.UTF8.GetString(decryptedData);
     }
 }
--- Wpf/Wpf/MainWindow.xaml.cs
+++ Wpf/Wpf/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using IdentityModel.OidcClient;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
 using System.Windows;
 
 namespace Wpf;
@@ -8,23 +13,57 @@ namespace Wpf;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private HttpClient _apiClient;
+    private OidcClient _oidcClient;
+
     public MainWindow()
     {
         InitializeComponent();
         Message.Text = "Logging in...";
     }
 
-    public void UserLoggedIn(LoginResult result)
+    public void DisplayMessage(string message)
     {
-        Message.Text = $"User logged in: {result.User.Identity.Name}";
+        Message.Text = message;
+        Show();
+    }
 
-        Details.Text =
-            $"""
-            Access Token: {result.AccessToken}
-            
-            Id Token: {result.IdentityToken}
-            """;
-        // Focus the UI
-        Activate();
+    public void Initialize(HttpClient client, OidcClient oidcClient)
+    {
+        _apiClient = client;
+        _oidcClient = oidcClient;
+    }
+
+    private async void CallApi_Click(object sender, RoutedEventArgs e)
+    {
+        var response = await _apiClient.GetAsync("");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var json = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+            Details.Text = json.RootElement.ToString();
+        }
+        else
+        {
+            Details.Text = $"Error: {response.ReasonPhrase}";
+        }
+    }
+
+    private async void Logout_Click(object sender, RoutedEventArgs e)
+    {
+        // TODO - persist id token, pass to logout endpoint
+        // That will involve expanding the persisted session
+        // You might as well make it a proper session object, with claims
+        // Add a login button to log back in
+        // Consider moving the login flow into the main window to cut down on the back and forth between App and MainWindow (and to allow login button to call the login code easily)
+        // Set the post logout redirect uri to also use the custom scheme. Have the app look for the post logout uri as well, and use that to signal that we are "logged out" (don't just directly set the string)
+        var url = await _oidcClient.PrepareLogoutAsync(new LogoutRequest());
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = url,
+            UseShellExecute = true
+        });
+        File.Delete("refresh_token");
+        Message.Text = "Logged out";
     }
 }
