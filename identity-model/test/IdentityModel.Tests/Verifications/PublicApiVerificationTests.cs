878343a7f60ed7780c868e56097d0d9eca4bc488
Damian Hickey <57436+damianh@users.noreply.github.com>
2024-11-03 19:23:13 +0100

    im: Add public api verification tests to guard against unintended breaking API changes.

---
+++ identity-model/test/IdentityModel.Tests/Verifications/PublicApiVerificationTests.cs
@@ -0,0 +1,16 @@
+﻿using PublicApiGenerator;
+
+namespace Duende.IdentityModel.Verifications;
+
+public class PublicApiVerificationTests
+{
+    [Fact]
+    public async Task VerifyPublicApi()
+    {
+        var publicApi = typeof(JwtClaimTypes).Assembly.GeneratePublicApi();
+        var settings = new VerifySettings();
+        settings.UniqueForTargetFrameworkAndVersion();
+        settings.UseDirectory("Verifications");
+        await Verify(publicApi, settings);
+    }
+}
