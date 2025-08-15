1101b09755a297e1baec634516ba8c3fafcfdb5c
Damian Hickey <57436+damianh@users.noreply.github.com>
2025-08-15 21:39:16 +0200

    Remove unused code. Don't need AngleSharp dependency any more.

--- access-token-management/test/AccessTokenManagement.Tests/Framework/TestBrowserClient.cs
+++ access-token-management/test/AccessTokenManagement.Tests/Framework/TestBrowserClient.cs
@@ -1,25 +1,29 @@
 // Copyright (c) Duende Software. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using System.Diagnostics;
 using System.Net;
-using AngleSharp.Dom;
-using AngleSharp.Html.Parser;
 
 namespace Duende.AccessTokenManagement.Framework;
 
 public class TestBrowserClient : HttpClient
 {
-    class CookieHandler : DelegatingHandler
+    public TestBrowserClient(HttpMessageHandler handler)
+        : this(new CookieHandler(handler))
     {
-        public CookieContainer CookieContainer { get; } = new();
-        public Uri CurrentUri { get; private set; } = null!;
-        public HttpResponseMessage LastResponse { get; private set; } = null!;
+    }
 
-        public CookieHandler(HttpMessageHandler next)
-            : base(next)
-        {
-        }
+    private TestBrowserClient(CookieHandler handler)
+        : base(handler)
+    {
+    }
+
+    private class CookieHandler(HttpMessageHandler next) : DelegatingHandler(next)
+    {
+        private CookieContainer CookieContainer { get; } = new();
+
+        public Uri CurrentUri { get; private set; } = null!;
+
+        public HttpResponseMessage LastResponse { get; private set; } = null!;
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
@@ -42,187 +46,5 @@ public class TestBrowserClient : HttpClient
 
             return response;
         }
-    }
-
-    private CookieHandler _handler;
-
-    public CookieContainer CookieContainer => _handler.CookieContainer;
-    public Uri CurrentUri => _handler.CurrentUri;
-    public HttpResponseMessage LastResponse => _handler.LastResponse;
-
-    public TestBrowserClient(HttpMessageHandler handler)
-        : this(new CookieHandler(handler))
-    {
-    }
-
-    private TestBrowserClient(CookieHandler handler)
-        : base(handler) => _handler = handler;
-
-    public Cookie? GetCookie(string name) => GetCookie(_handler.CurrentUri.ToString(), name);
-
-    public Cookie? GetCookie(string uri, string name) => _handler.CookieContainer.GetCookies(new Uri(uri)).Cast<Cookie>().Where(x => x.Name == name).FirstOrDefault();
-
-    public void RemoveCookie(string name) => RemoveCookie(CurrentUri.ToString(), name);
-
-    public void RemoveCookie(string uri, string name)
-    {
-        var cookie = CookieContainer.GetCookies(new Uri(uri)).Cast<Cookie>().Where(x => x.Name == name).FirstOrDefault();
-        if (cookie != null)
-        {
-            cookie.Expired = true;
-        }
-    }
-
-    public async Task FollowRedirectAsync()
-    {
-        LastResponse.StatusCode.ShouldBe((HttpStatusCode)302);
-        var location = LastResponse.Headers.Location!.ToString();
-        await GetAsync(location);
-    }
-
-    public Task<HttpResponseMessage> PostFormAsync(HtmlForm form) => PostAsync(form.Action, new FormUrlEncodedContent(form.Inputs));
-
-    public Task<HtmlForm> ReadFormAsync(string? selector = null) => ReadFormAsync(LastResponse, selector);
-
-    public async Task<HtmlForm> ReadFormAsync(HttpResponseMessage response, string? selector = null)
-    {
-        response.StatusCode.ShouldBe(HttpStatusCode.OK);
-
-        var htmlForm = new HtmlForm();
-
-        var html = await response.Content.ReadAsStringAsync();
-
-        var parser = new HtmlParser();
-        var dom = parser.ParseDocument(html);
-
-        var form = dom.QuerySelector(selector ?? "form");
-        form.ShouldNotBeNull();
-
-        var postUrl = form.GetAttribute("action")!;
-        if (!postUrl.StartsWith("http", StringComparison.OrdinalIgnoreCase))
-        {
-            if (postUrl.StartsWith("/"))
-            {
-                postUrl = CurrentUri.Scheme + "://" + CurrentUri.Authority + postUrl;
-            }
-            else
-            {
-                postUrl = CurrentUri + postUrl;
-            }
-        }
-        htmlForm.Action = postUrl;
-
-
-        var data = new Dictionary<string, string?>();
-
-        var inputs = form.QuerySelectorAll("input");
-        foreach (var input in inputs)
-        {
-            var name = input.GetAttribute("name")!;
-            var value = input.GetAttribute("value");
-
-            data[name] = value;
-        }
-        htmlForm.Inputs = data;
-
-        return htmlForm;
-    }
-
-
-    private Task<string> ReadElementTextAsync(string selector) => ReadElementTextAsync(LastResponse, selector);
-
-    private async Task<string> ReadElementTextAsync(HttpResponseMessage response, string selector)
-    {
-        var html = await response.Content.ReadAsStringAsync();
-        var parser = new HtmlParser();
-        var dom = parser.ParseDocument(html);
-        var element = dom.QuerySelector(selector)!;
-        return element.Text();
-    }
-
-    private Task<string> ReadElementAttributeAsync(string selector, string attribute) => ReadElementAttributeAsync(LastResponse, selector, attribute);
-
-    private async Task<string> ReadElementAttributeAsync(HttpResponseMessage response, string selector, string attribute)
-    {
-        var html = await response.Content.ReadAsStringAsync();
-        var parser = new HtmlParser();
-        var dom = parser.ParseDocument(html);
-        var element = dom.QuerySelector(selector)!;
-        return element.GetAttribute(attribute)!;
-    }
-
-    private Task AssertExistsAsync(string selector) => AssertExistsAsync(LastResponse, selector);
-
-    private async Task AssertExistsAsync(HttpResponseMessage response, string selector)
-    {
-        response.StatusCode.ShouldBe(HttpStatusCode.OK);
-
-        var html = await response.Content.ReadAsStringAsync();
-        var parser = new HtmlParser();
-        var dom = parser.ParseDocument(html);
-        var element = dom.QuerySelector(selector);
-        element.ShouldNotBeNull();
-    }
-
-    public Task AssertNotExistsAsync(string selector) => AssertNotExistsAsync(selector);
-
-    public async Task AssertNotExistsAsync(HttpResponseMessage response, string selector)
-    {
-        response.StatusCode.ShouldBe(HttpStatusCode.OK);
-
-        var html = await response.Content.ReadAsStringAsync();
-        var parser = new HtmlParser();
-        var dom = parser.ParseDocument(html);
-        var element = dom.QuerySelector(selector);
-        element.ShouldNotBeNull();
-    }
-
-    public Task AssertErrorPageAsync(string? error = null) => AssertErrorPageAsync(LastResponse, error);
-    public async Task AssertErrorPageAsync(HttpResponseMessage response, string? error = null)
-    {
-        response.StatusCode.ShouldBe(HttpStatusCode.OK);
-        await AssertExistsAsync(response, ".error-page");
-
-        if (!string.IsNullOrWhiteSpace(error))
-        {
-            var errorText = await ReadElementTextAsync(response, ".alert.alert-danger");
-            errorText.ShouldContain(error);
-        }
-    }
-
-    public Task AssertValidationErrorAsync(string? error = null) => AssertValidationErrorAsync(error);
-    public async Task AssertValidationErrorAsync(HttpResponseMessage response, string? error = null)
-    {
-        response.StatusCode.ShouldBe(HttpStatusCode.OK);
-        await AssertExistsAsync(response, ".validation-summary-errors");
-
-        if (!string.IsNullOrWhiteSpace(error))
-        {
-            var errorText = await ReadElementTextAsync(response, ".validation-summary-errors");
-            errorText.ToLowerInvariant().ShouldContain(error.ToLowerInvariant());
-        }
-    }
-}
-
-[DebuggerDisplay("{Action}, Inputs: {Inputs.Count}")]
-public class HtmlForm
-{
-    public HtmlForm(string? action = null) => Action = action;
-
-    public string? Action { get; set; }
-    public Dictionary<string, string?> Inputs { get; set; } = new();
-
-    public string? this[string key]
-    {
-        get
-        {
-            if (Inputs.ContainsKey(key))
-            {
-                return Inputs[key];
-            }
-
-            return null;
-        }
-        set => Inputs[key] = value;
     }
 }
