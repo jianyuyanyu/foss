9aa3776707609a0d38c553b7b1437c8beec6b288
pgermishuys <pieter@pieterg.com>
2025-09-25 14:43:06 +0200

    Use an MD5 Hex Encode String for Cache Keys

--- access-token-management/src/AccessTokenManagement/Internal/DefaultClientCredentialsCacheKeyGenerator.cs
+++ access-token-management/src/AccessTokenManagement/Internal/DefaultClientCredentialsCacheKeyGenerator.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Duende Software. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.Extensions.Options;
 
 namespace Duende.AccessTokenManagement.Internal;
@@ -8,7 +10,7 @@ namespace Duende.AccessTokenManagement.Internal;
 /// <summary>
 /// The logic to generate a cache key. Defaults to a key based on <see cref="ClientCredentialsTokenManagementOptions.CacheKeyPrefix"/>, client name, scope, and resource.
 /// Customize this if you add additional <see cref="TokenRequestParameters.Parameters"/> to your TokenRequest that
-/// impact how this should be cached. 
+/// impact how this should be cached.
 /// </summary>
 /// <param name="options"></param>
 internal class DefaultClientCredentialsCacheKeyGenerator(
@@ -18,9 +20,17 @@ internal class DefaultClientCredentialsCacheKeyGenerator(
         ClientCredentialsClientName clientName,
         TokenRequestParameters? parameters = null)
     {
-        var scopePart = "s_" + parameters?.Scope;
-        var resourcePart = "r_" + parameters?.Resource;
+        var scopePart = "s_" + GetCacheKey(parameters?.Scope ?? string.Empty);
+        var resourcePart = "r_" + GetCacheKey(parameters?.Resource ?? string.Empty);
 
-        return ClientCredentialsCacheKey.Parse(options.Value.CacheKeyPrefix + clientName + "::" + scopePart + "::" + resourcePart);
+        return ClientCredentialsCacheKey.Parse(options.Value.CacheKeyPrefix + clientName + "::" + scopePart + "::" +
+                                               resourcePart);
+    }
+
+    private static string GetCacheKey(string value)
+    {
+        using var md5 = MD5.Create();
+        var hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(value));
+        return Convert.ToHexString(hashBytes);
     }
 }
