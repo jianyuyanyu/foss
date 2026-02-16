c830c158ff72e3f7368d7a56db227dbfc93bab2a
Damian Hickey <57436+damianh@users.noreply.github.com>
2026-02-16 14:31:23 +0100

    Fix import ordering in Parameters.cs

--- identity-model/src/IdentityModel/Client/Messages/Parameters.cs
+++ identity-model/src/IdentityModel/Client/Messages/Parameters.cs
@@ -1,9 +1,9 @@
 // Copyright (c) Duende Software. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Duende.IdentityModel.Internal;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Duende.IdentityModel.Client;
 
